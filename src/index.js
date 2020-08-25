7f9fd1c3253fda228b672beaf90f40df2fd2586e
rwarneke <rowanwarneke@gmail.com>
2020-08-25 17:47:49 +1000

    build board with piece display support

--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function initialiseChessBoard() {
+    var board = Array(8);
+    for (var i = 0; i < 8; i++) {
+        board[i] = Array(8).fill(null);
+    }
+    board[0][0] = new King(2);
+    board[7][7] = new King(1);
+    return board;
+}
+
+class Piece {
+    constructor(player, iconUrl) {
+        this.player = player;
+        this.iconUrl = iconUrl;
+        this.style = {
+            backgroundImage: "url('" + iconUrl + "')"
+        };
+    }
+}
+
+class King extends Piece {
+    constructor(player) {
+        var url = player === 1 ? "https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg" : "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg";
+        super(player, url);
+    }
+}
+
+function Square(props) {
+    return (
+        <button
+            className="square"
+            style={props.style}></button>
+    )
+}
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: initialiseChessBoard()
+        };
+    }
+
+    renderSquare(i, j) {
+        let piece = this.state.squares[i][j];
+        return(
+            <Square
+                style = {piece ? piece.style : null}
+            />
+        )
+    }
+
+    render() {
+        const board = []
+        for (let i = 0; i < 8; i++) {
+            const row = [];
+            for (let j = 0; j < 8; j++) {
+                row.push(this.renderSquare(i, j));
+            }
+            board.push(<div className="board-row">{row}</div>);
+        }
+        return(
+            <div className="board">
+                {board}
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDom.render(
+    <Game />,
+    document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
